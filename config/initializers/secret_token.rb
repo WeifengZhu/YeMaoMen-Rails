ebdaa61dea1f73c0dbf56e095785ef2b5d4ea673
WeifengZhu <weifeng.zhu.ru@gmail.com>
2013-10-18 23:00:25 +0800

    first commit!!!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yemaomen::Application.config.secret_token = '9977137b3912a0031ca705f9682ef9d0aa799ca823e2c86d90b7629309dfc2352dbe92e0a74ad5541c74d5e5545657344d4246cf4ff41365d3920d04c82e0f50'
