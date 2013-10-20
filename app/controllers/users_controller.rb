166199b05194169e67bf96fb9611fb7daa145b0c
WeifengZhu <weifeng.zhu.ru@gmail.com>
2013-10-21 00:14:25 +0800

    complete User create api

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,11 +2,16 @@ class UsersController < ApplicationController
   # 响应xxxx.json的请求
   respond_to :json
 
+  # POST /users
   def create
     # 这里这么写的好处是：只要客户端把对应的参数放在一个user的key下，后台这里就不需要再枚举一边key值了。
     @user = User.new(params[:user])
-    @user.save
-    # 为毛不用指定返回啥？
-    # because Rails will automatically detect and render the associated view once the template is defined. 
+    if @user.save
+      # app/views/users/create.rabl
+      render "users/create"
+    else
+      # 直接返回，不通过模板，并且加上对应的status code。
+      render json: {'failure_reason' => @user.errors.full_messages.first }.to_json, status: 400
+    end
   end
 end
