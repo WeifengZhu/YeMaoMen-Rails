84f8b9bbca9ec4358d12b3d1ad03243daec442b8
WeifengZhu <weifeng.zhu.ru@gmail.com>
2013-10-20 16:51:08 +0800

    create identify_token before user been saved

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,4 +17,17 @@
 
 class User < ActiveRecord::Base
   attr_accessible :allow_browse, :avatar_url, :bio, :last_active_time, :location, :password_digest, :score, :username
+
+  # call create_remember_token before save
+  before_save :create_identify_token
+
+  pirvate:
+
+    # 目前的处理逻辑是用户被保存进数据库之前产生一个identify_token并保存进数据库，用户登陆并不会刷新这个token，所以不能保证用户只能登陆
+    # 一台设备，后续如果有需要可以加上这个限制。
+    def create_identify_token
+      # without self the assignment would create a local variable called identify_token, 
+      # which isn’t what we want at all.
+      self.identify_token = SecureRandom.urlsafe_base64
+    end
 end
