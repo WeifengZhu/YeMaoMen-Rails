2d651adad7f8358f040ac6b4efef8a22e2a79b7e
WeifengZhu <weifeng.zhu.ru@gmail.com>
2013-10-21 21:21:20 +0800

    rename file create.rabl to user.rabl

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,8 +9,8 @@ class UsersController < ApplicationController
     # 这里为什么用save，而不用save!，因为save如果成功返回true，如果失败会返回false，刚好放这。save!失败的话则会抛出异常
     # 不适用于这个if...else...end结构，应该用异常处理的那个结构。
     if @user.save
-      # app/views/users/create.rabl
-      render 'users/create'
+      # app/views/users/user.rabl
+      render 'users/user'
     else
       # 直接返回json代码参考
       # render json: {'failure_reason' => @user.errors.full_messages.first }.to_json, status: 400
