cb80d29561bfd76bae860415f1300e42547ffe35
WeifengZhu <weifeng.zhu.ru@gmail.com>
2013-10-23 13:28:44 +0800

    add schema info to Topic and Post models

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id               :integer         not null, primary key
+#  topic_id         :integer
+#  user_id          :integer
+#  content          :text
+#  like_count       :integer
+#  reply_to_post_id :integer
+#  created_at       :datetime        not null
+#  updated_at       :datetime        not null
+#
+
 class Post < ActiveRecord::Base
   attr_accessible :content, :like_count, :reply_to_post_id, :topic_id, :user_id
 end
