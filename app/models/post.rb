4b61b309e2768d4ea8946f4e541b29e9856a4920
WeifengZhu <weifeng.zhu.ru@gmail.com>
2013-10-28 16:44:10 +0800

    add conter_cache:true to Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -18,11 +18,15 @@ class Post < ActiveRecord::Base
   
   # A belongs_to association sets up a one-to-one connection with another model, such that each instance of the declaring 
   # model “belongs to” one instance of the other model. 在posts表中要有topic_id这个字段。
-  belongs_to :topic
+  belongs_to :topic, counter_cache: true
   belongs_to :user
   
   # self join
   # 这么设置好之后，可以通过@post.reply_to_post来获取这个post所回复的post。
+  # 如果有返回这个post对象，如果没有，则返回nil。
+  # @post.reply_to_post默认是会用缓存的，即：
+  # If the associated object has already been retrieved from the database for this object, the cached version will be returned. 
+  # To override this behavior (and force a database read), pass true as the force_reload argument(@post.reply_to_post(true)).
   has_one :reply_to_post, class_name: "Post", foreign_key: "reply_to_post_id"
     
 end
--- app/views/topics/index.rabl
+++ app/views/topics/index.rabl
@@ -1,3 +1,5 @@
 collection @topics
 attributes :id, :title, :description
-node(:postCount) { |topic| topic.posts.count }
+# 这里其实也可以用count方法，效果是一样的。这里用size方法，会在数据库层面执行COUNT(*)，因为在Post模型中设置了counter_cache: ture，所以
+# 用这个的性能会好一点。
+node(:postCount) { |topic| topic.posts.size }
