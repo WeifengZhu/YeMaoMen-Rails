b12c74a0b90451ce5cf4efb98b721c8388e38a3c
WeifengZhu <weifeng.zhu.ru@gmail.com>
2013-10-28 21:59:28 +0800

    update self join for Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -22,11 +22,15 @@ class Post < ActiveRecord::Base
   belongs_to :user
   
   # self join
-  # 这么设置好之后，可以通过@post.reply_to_post来获取这个post所回复的post。
-  # 如果有返回这个post对象，如果没有，则返回nil。
+  # 这么设置好之后，可以通过@post.reply_to_post来获取这个post所回复的post，通过@post.re_posts来获取回复这个Post的posts。
+  # 如果有，则返回这个post对象，如果没有，则返回nil。可以用以下表达式来判断关联对象是否存在：@post.reply_to_post.nil?, @post.re_posts.nil?。
   # @post.reply_to_post默认是会用缓存的，即：
   # If the associated object has already been retrieved from the database for this object, the cached version will be returned. 
   # To override this behavior (and force a database read), pass true as the force_reload argument(@post.reply_to_post(true)).
-  has_one :reply_to_post, class_name: "Post", foreign_key: "reply_to_post_id"
+  #
+  # If you set the :dependent option to :nullify, then deleting this object will set the foreign key in the association object to NULL.
+  # foreign_key为NULL和空是有区别的：NULL证明之前有过，只是后续被删掉了，空证明一直都没有过。
+  has_many :re_posts, :class_name => "Post", foreign_key: "reply_to_post_id", dependent: :nullify
+  belongs_to :reply_to_post, :class_name => "Post"
     
 end
