d85016ac6ca0ac3a41ade2a799c463fc8ab71d42
WeifengZhu <weifeng.zhu.ru@gmail.com>
2013-11-12 14:22:46 +0800

    add data to development database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# 创建用户
+(1..10).each do |i|
+  User.create(username: "user#{i}", password: '000000', password_confirmation: '000000', score: i)
+end
+
+# 常见闲扯猫聊
+(1..40).each do |i|
+  Post.create(user_id: 1, content: 'The content of the post.', like_count: i )
+end
+
+# 创建话题
+(1..5).each do |i|
+  Topic.create(title: "topic#{i}", description: "description#{i}")
+end
+
+# 创建话题下猫聊
+(1..40).each do |i|
+  Post.create(user_id: 2, topic_id: 1, content: 'The content of the post.', like_count: i, )
+end
+
