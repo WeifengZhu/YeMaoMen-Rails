1e309d197b837f21225580b0966b38771c5f0a0a
WeifengZhu <weifeng.zhu.ru@gmail.com>
2013-12-30 16:05:49 +0800

    给post的like_count加上默认值0

--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131106125321) do
+ActiveRecord::Schema.define(:version => 20131230073750) do
 
   create_table "posts", :force => true do |t|
     t.integer  "topic_id"
     t.integer  "user_id"
     t.text     "content"
-    t.integer  "like_count"
+    t.integer  "like_count",       :default => 0
     t.integer  "reply_to_post_id"
-    t.datetime "created_at",       :null => false
-    t.datetime "updated_at",       :null => false
+    t.datetime "created_at",                      :null => false
+    t.datetime "updated_at",                      :null => false
   end
 
   create_table "topics", :force => true do |t|
